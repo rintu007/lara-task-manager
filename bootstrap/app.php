7ed7965749642c64bf4a8dccb13265913a1b8d20
Rintu Kumar Chowdhury <rintuchy.cse@gmail.com>
2025-09-14 17:00:29 +0600

    Update bootstrap/app.php for API Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,11 +8,14 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // Add this section
+        $middleware->group('api', [
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
